a4995c2578344ee69d9e35ba6f4c4e34a507e0de
masuda manato <2301100047ir@cyber-u.ac.jp>
2026-06-10 22:57:02 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://cu2301100047:2301100047@ac-wxltvtg-shard-00-00.op2bauu.mongodb.net:27017,ac-wxltvtg-shard-00-01.op2bauu.mongodb.net:27017,ac-wxltvtg-shard-00-02.op2bauu.mongodb.net:27017/?ssl=true&replicaSet=atlas-wwtd0m-shard-0&authSource=admin&appName=Cluster0";
+const uri = "DUMMY";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
