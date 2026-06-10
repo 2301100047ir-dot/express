47b0c84eaacabde48fdc851626d7a2207b771a76
masuda manato <2301100047ir@cyber-u.ac.jp>
2026-06-10 21:55:01 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-// レスポンスのデータ（ノート全件）
-const responseObjectDataAll = {
-textObject1 : {
-id: 1,
-title: 'ノート１のタイトルです',
-subTitle: 'ノート１のサブタイトルです',
-bodyText: 'ノート１の本文です'
-},
-textObject2 : {
-id: 2,
-title: 'ノート２のタイトルです',
-subTitle: 'ノート２のサブタイトルです',
-bodyText: 'ノート２の本文です'
-},
-};
 
-/**
-* メモを全件取得するAPI
-* @returns {Object[]} data
-* @returns {number} data.id - ID
-* @returns {string} data.title - タイトル
-* @returns {string} data.text - 内容
-*/
-router.get('/', function (req, res, next) {
-// 全件取得して返す
-res.json(responseObjectDataAll);
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://cu2301100047:2301100047@ac-wxltvtg-shard-00-00.op2bauu.mongodb.net:27017,ac-wxltvtg-shard-00-01.op2bauu.mongodb.net:27017,ac-wxltvtg-shard-00-02.op2bauu.mongodb.net:27017/?ssl=true&replicaSet=atlas-wwtd0m-shard-0&authSource=admin&appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
 })
 
 module.exports = router;
